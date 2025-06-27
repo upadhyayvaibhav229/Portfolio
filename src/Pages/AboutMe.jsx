a87b58d6e8ae56e3c4ab5920360b779c11cc952f
upadhyayvaibhav229 <upadhyayvaibhav229@gmail.com>
2025-06-27 17:14:42 +0530

    cpmt

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -11,7 +11,7 @@ const AboutMe = () => {
     <div id='about' className='container sm:min-w-full xl:min-w-full min-w-full py-10 dark:bg-slate-900'>
       <div className="flex flex-col items-center justify-center gap-8 text-center">
         <img src={img.flower} alt="flower" />
-        <p className='text-white md:text-4xl text-md text-center font-bold dark:text-white w-4/5'>
+        <p className='text-black md:text-4xl text-md text-center font-bold dark:text-white w-4/5'>
           I'm Vaibhav Upadhyay, a passionate MERN Stack Developer crafting scalable web applications with clean code and creative solutions. Currently based in the vibrant city of Kandivali, Mumbai, I work with clients across the globe and am always ready to take on new challenges in the world of full-stack development.
         </p>
       </div>
